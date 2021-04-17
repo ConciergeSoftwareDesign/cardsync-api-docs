875b16e34c739aa0f110bddde5f784db0045ab9c
Rick Duggan <rick@conciergesoftwaredesign.com>
2021-04-17 08:15:25 -0700

    add creation of zip archive to build script; do not allow build artifact in git

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 set -o errexit #abort if any command fails
 docker pull slatedocs/slate
 docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate
+# zip does not remove entries, so we must delete the zip first
+rm -f ./cardsync-api.zip
+zip -r cardsync-api build
