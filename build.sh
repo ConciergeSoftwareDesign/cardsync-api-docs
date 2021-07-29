ee02bb8c9305363bb84bb8908965e126e3e8b70c
Rick Duggan <rick@conciergesoftware.com>
2021-07-29 13:44:23 -0700

    update build script and change to Paylani logo

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+# Usasge -- build.sh [api-name], e.g. build.sh evoke-api or cardsync-api
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 docker pull slatedocs/slate
 docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate
 # zip does not remove entries, so we must delete the zip first
-rm -f ./cardsync-api.zip
-zip -r cardsync-api build
+rm -f ./$1.zip
+zip -r ./$1.zip build
